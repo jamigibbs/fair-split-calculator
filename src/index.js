e61eb5a59a4e7202f1f7c98d4f56283a990db796
Jami Gibbs <jami0821@gmail.com>
2016-11-22 15:51:23 -0600

    Adding ga

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactGA from 'react-ga';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+ReactGA.initialize('UA-3363703-50');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
 ReactDOM.render(
-  <App />,
+  <App onUpdate={logPageView} />,
   document.getElementById('root')
 );
