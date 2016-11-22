672a0b137f575160785e3a100f448cac5c06e9c5
Jami Gibbs <jami0821@gmail.com>
2016-11-22 16:11:03 -0600

    Remove react-ga

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ReactGA from 'react-ga';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-ReactGA.initialize('UA-3363703-50');
-
-function logPageView() {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
-}
-
 ReactDOM.render(
-  <App onUpdate={logPageView} />,
+  <App />,
   document.getElementById('root')
 );
